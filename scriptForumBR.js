a2c802606ddce297cb92906232d5c392e089388d
Vladyslav Tkach <superovlad39@gmail.com>
2026-04-24 22:49:19 +0200

    project initialization

---
+++ scriptForumBR.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Script for Forum Black Russia
+// @namespace    http://tampermonkey.net/
+// @version      2026-04-24
+// @description  try to take over the world!
+// @author       Vladyslav Tkach
+// @match        https://*/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+'use strict'
+

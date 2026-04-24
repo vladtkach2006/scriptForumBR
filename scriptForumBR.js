901d51990ca520a175006487f26c1df7540a9bce
Vladyslav Tkach <superovlad39@gmail.com>
2026-04-24 22:54:13 +0200

    update description

--- scriptForumBR.js
+++ scriptForumBR.js
@@ -2,7 +2,7 @@
 // @name         Script for Forum Black Russia
 // @namespace    http://tampermonkey.net/
 // @version      2026-04-24
-// @description  try to take over the world!
+// @description  Quick response script for the Black Russia mobile game forum 
 // @author       Vladyslav Tkach
 // @match        https://*/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==

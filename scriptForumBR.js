88ee68209ec8ec74983c5cd31b5ee8ff173acbb0
Vladyslav Tkach <superovlad39@gmail.com>
2026-04-24 23:42:16 +0200

    update namespace and match

--- scriptForumBR.js
+++ scriptForumBR.js
@@ -2,11 +2,10 @@
 // @name         Script for Forum Black Russia
 // @namespace    http://tampermonkey.net/
 // @version      2026-04-24
-// @description  Quick response script for the Black Russia mobile game forum 
+// @description  Quick response script for the Black Russia mobile game forum
 // @author       Vladyslav Tkach
-// @match        https://*/*
+// @match        https://forum.blackrussia.online/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
-'use strict'
 

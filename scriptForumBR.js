f58d7c24a55c3a7168cd761a2f3b205a90ab472c
Vladyslav Tkach <superovlad39@gmail.com>
2026-04-25 02:23:41 +0200

    Fetching data

--- scriptForumBR.js
+++ scriptForumBR.js
@@ -6,13 +6,52 @@
 // @author       Vladyslav Tkach
 // @match        https://forum.blackrussia.online/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
-// @grant        none
+// @grant        GM_xmlhttpRequest
+// @connect      raw.githubusercontent.com
 // ==/UserScript==
 
 ;(function () {
 	'use strict'
-	const startMainScript = () => {}
-	if (document.readyState === 'loading')
-		document.addEventListener('DOMContentLoaded', startMainScript)
-	else startMainScript()
+
+	const startMainScript = dataArray => {}
+
+	let isDomReady = false
+	let isDataReady = false
+	let loadedData = null
+
+	const checkAndRun = () => {
+		if (isDomReady && isDataReady) startMainScript(loadedData)
+	}
+
+	if (document.readyState === 'loading') {
+		document.addEventListener('DOMContentLoaded', () => {
+			isDomReady = true
+			checkAndRun()
+		})
+	} else {
+		isDomReady = true
+		checkAndRun()
+	}
+
+	const DATA_URL =
+		'https://raw.githubusercontent.com/vladtkach2006/dataForScriptForumBR/main/date.json'
+
+	GM_xmlhttpRequest({
+		method: 'GET',
+		url: DATA_URL,
+		onload: response => {
+			if (response.status === 200) {
+				try {
+					loadedData = JSON.parse(response.responseText)
+					isDataReady = true
+					checkAndRun()
+					console.log(loadedData)
+				} catch (error) {
+					console.error('Ошибка парсинга JSON', error)
+				}
+			} else {
+				console.error('Ошибка загрузки файла', response.status)
+			}
+		}
+	})
 })()
